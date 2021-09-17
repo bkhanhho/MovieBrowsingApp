4f80a5fb0ba65770269773cf41d2dc3cc207dee3
bkhanhho <buukhanh.ueh@gmail.com>
2021-09-17 16:25:06 -0400

    Second commit
    Adding part 2

--- flixster/ViewController.swift
+++ flixster/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         
         // Do any additional setup after loading the view.
-        print("hello")
+        //print("hello")
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -36,9 +36,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                     let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
 
                 self.movies = dataDictionary["results"] as! [[String:Any]]
-                
                 self.tableView.reloadData()
-                print(dataDictionary)
+                
+                //print(dataDictionary)
+                
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
                     // TODO: Reload your table view data
@@ -70,6 +71,23 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.posterView.af_setImage(withURL: posterUrl!)
         
         return cell
+    }
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        print("Loading up the details screen")
+        
+        //Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //Pass selected movie to details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 }
